79e5a763efd22384cb63af2026f8ede8ce9e76f2
Aatitkarki <52074509+Aatitkarki@users.noreply.github.com>
2021-02-22 23:41:40 +0545

    new update

--- fccScript.zsh
+++ fccScript.zsh
@@ -3,7 +3,9 @@ folder=$(find lib/main.dart | grep -i "lib/main.dart")
 if [ "$folder" = 'lib/main.dart' ]
 then
 git clone https://github.com/Aatitkarki/flutter_clean_code_template.git
+cd flutter_clean_code_template
 git checkout origin/simpleCleanCode
+cd ..
 cp -r flutter_clean_code_template/lib .
 rm -rf flutter_clean_code_template
 echo "Successfully implemented flutter clean code architecture";
