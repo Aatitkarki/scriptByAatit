32c3e2811e40d8e184c4789540fe778a67f7f4ec
aatitkarki <aatitkarki@gmail.com>
2021-09-18 20:20:19 +0545

    added auto package add functionality"

--- fccScript.sh
+++ fccScript.sh
@@ -6,6 +6,8 @@ git clone https://github.com/Aatitkarki/flutter_clean_code_template.git
 cp -r flutter_clean_code_template/lib .
 rm -rf flutter_clean_code_template
 echo "Successfully implemented flutter clean code architecture";
+echo "Adding packages";
+flutter pub add get && flutter pub add cached_network_image && flutter pub add http && flutter pub add google_fonts && flutter pub add get_it && flutter pub add equatable && flutter pub add dartz && flutter pub add json_annotation && flutter pub add hive && flutter pub add hive_flutter && flutter pub add connectivity
 else
   echo "Cannot recognize this path as flutter project";
 fi
--- fccScript.zsh
+++ fccScript.zsh
@@ -9,6 +9,8 @@ cd ..
 cp -r flutter_clean_code_template/lib .
 rm -rf flutter_clean_code_template
 echo "Successfully implemented flutter clean code architecture";
+echo "Adding packages";
+flutter pub add get && flutter pub add cached_network_image && flutter pub add http && flutter pub add google_fonts && flutter pub add get_it && flutter pub add equatable && flutter pub add dartz && flutter pub add json_annotation && flutter pub add hive && flutter pub add hive_flutter && flutter pub add connectivity
 else
   echo "Cannot recognize this path as flutter project";
 fi
